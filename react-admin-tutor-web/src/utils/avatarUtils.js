e09ca53238527398ed394c486f0904169f473eba
VhulendaSmoke <vhulendamashamba4@gmail.com>
2026-06-03 18:01:29 +0200

    Addin Crud to notes

--- react-admin-tutor-web/src/utils/avatarUtils.js
+++ react-admin-tutor-web/src/utils/avatarUtils.js
@@ -1,2 +1,2 @@
 export const getAvatarUrl = (seed) =>
-  `https://api.multiavatar.com/${encodeURIComponent(seed || 'default')}.svg`;
+  `https://api.dicebear.com/9.x/fun-emoji/svg?seed=${encodeURIComponent(seed || 'default')}`;
